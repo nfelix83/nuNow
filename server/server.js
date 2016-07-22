85f4497ed21d9cd9d55d72c5e26d92e977651e98
Noel Felix <nfelix83@gmail.com>
2016-07-22 14:52:03 -0700

    (feat) Rough scraper working

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 let express   = require('express'),
-    mongoose  = require('mongoose');
+    mongoose  = require('mongoose'),
+    scrape    = require('../client/js/scraper');
 
 let app = express();
 
 require('./config/middleware.js')(app, express);
 
-mongoose.connect(process.env.DB_PATH);
+// mongoose.connect(process.env.DB_PATH);
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
 
 let request = require('request');
 
-request(process.env.FB_URL, (err, res, body) => {
-  console.log('HERE', body);
-});
+// request(process.env.FB_URL, (err, res, body) => {
+//   console.log('HERE', body);
+// });
 
 module.exports = app;
+
+scrape()
